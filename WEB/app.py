7329f7b9847df1e92c181defb8e4fde545b3d667
Alexey <lehacomp16@gmail.com>
2026-08-12 19:04:17 +0300

    fix: add stronger user-agent checks for uptime monitors

--- WEB/app.py
+++ WEB/app.py
@@ -91,7 +91,12 @@ def check_user_agent():
 )  # Limit to 25 requests per minute per IP x 4 workers = 100 requests per minute
 def index() -> str:
     user_agent = request.headers.get("User-Agent", "").lower()
-    if "uptimerobot" in user_agent or "headless" in user_agent:
+    if (
+        "uptimerobot" in user_agent
+        or "render" in user_agent
+        or "headless" in user_agent
+        or not user_agent
+    ):
         return "ok"
     config_api_key = getattr(Config, "WEATHER_API_KEY", None)
     if "db_session" not in g:
